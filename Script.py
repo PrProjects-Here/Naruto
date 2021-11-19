3e2c2541e076dfbe9bfbda2f75a348d306332c90
Piyush Projects <80516641+piyushraj00@users.noreply.github.com>
2021-11-19 08:32:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -22,11 +22,8 @@ Here Is The Help For MY COMMANDS**"""
 
 📡  Server : [Heroku](heroku.com)**"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
-
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+[Click Here](t.me/SourceCodetg) 
+"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
@@ -43,7 +40,7 @@ Here Is The Help For MY COMMANDS**"""
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- Naruto Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
@@ -51,7 +48,7 @@ Here Is The Help For MY COMMANDS**"""
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/TheNarutoroRoBot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -78,7 +75,7 @@ Here Is The Help For MY COMMANDS**"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of Naruto
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specifed user.</code>
