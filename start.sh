fbfdeb27b4b7c016695488a4c196ca742485a197
Piyush Projects <80516641+piyushraj00@users.noreply.github.com>
2021-11-20 03:43:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/PrProjects-Here/Naruto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Naruto
+  git clone $UPSTREAM_REPO
 fi
-cd /EvaMaria
+cd /Naruto
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
