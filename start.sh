1abe1b5af5fc66a6c3939c73b3a5921c61bd89e2
Piyush Projects <80516641+piyushraj00@users.noreply.github.com>
2021-11-20 04:11:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PrProjects-Here/Naruto /Naruto
+  git clone https://github.com/PrProjects-Here/Naruto.git /Naruto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Naruto
