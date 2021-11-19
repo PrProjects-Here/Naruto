01a8472a9ccd4304819664c1afe962cc7ec2512c
Piyush Projects <80516641+piyushraj00@users.noreply.github.com>
2021-11-20 03:54:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PrProjects-Here/Naruto /Naruto
+  git clone https://github.com/EvaMariaTg/EvaMaria /Naruto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Naruto
 fi
-cd /Naruto
+cd /EvaMarie
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
