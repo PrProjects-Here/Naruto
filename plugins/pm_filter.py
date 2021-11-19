f52eafb779d09a46bb13c92cd64638d2330d8b2d
Piyush Projects <80516641+piyushraj00@users.noreply.github.com>
2021-11-20 03:21:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -440,7 +440,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
-        ], [InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''), InlineKeyboardButton('📌 Other Bots', url='telegram.me/YoutubeVideoDownloaderService/70)]]
+        ], [InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''), InlineKeyboardButton('📌 Other Bots', url='telegram.me/YoutubeVideoDownloaderService/70')]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
